e231a6e6c9a28053c96e7da6f120dc3dd17c9d6b
gcpearse <glen.c.pearse@gmail.com>
2024-03-13 00:37:52 +0000

    refactoring

--- week-0/lecture/calculator.py
+++ week-0/lecture/calculator.py
@@ -1,6 +1,6 @@
 def main():
-  sum_ints()
-  divide_floats()
+  print(sum_ints())
+  print(divide_floats())
   n = int(input("Choose a number to square: "))
   print(find_square(n))
 
@@ -9,14 +9,14 @@ def sum_ints():
   a = int(input("Enter a value for a: "))
   b = int(input("Enter a value for b: "))
   c = (a + b)
-  print(f"{c:,}")
+  return f"{c:,}"
 
 
 def divide_floats():
   x = float(input("Enter a value for x: "))
   y = float(input("Enter a value for y: "))
   z = round(x / y, 2)
-  print(z)
+  return z
 
 
 def find_square(n):
