c5a121eda5b2c04ee66d1a949c55257059ffaefa
gcpearse <glen.c.pearse@gmail.com>
2024-03-13 01:19:04 +0000

    adds completed problem sets for week 1

---
+++ week-1/problem-sets/interpreter.py
@@ -0,0 +1,28 @@
+# Prompt the user for an arithmetic expression
+# Output the result as floating-point value
+# Ensure the result is rounded to 1dp
+
+
+def main():
+  expression = input("Enter an expression: ").strip()
+  result = calculate(expression)
+  print(result)
+
+
+def calculate(expression: str):
+  x, y, z = expression.split(" ")
+  x = float(x)
+  z = float(z)
+  match y:
+    case "+":
+      result = x + z
+    case "-":
+      result = x - z
+    case "*":
+      result = x * z
+    case "/":
+      result = x / z
+  return round(result, 1)
+
+
+main()
