45fe31e7d9cecd936aa15bf170a712053e2a74b1
gcpearse <glen.c.pearse@gmail.com>
2024-03-15 17:43:31 +0000

    adds lecture examples for simple loops

---
+++ week-2/lecture/cat.py
@@ -0,0 +1,18 @@
+def main():
+  n = get_number()
+  miaow(n)
+
+
+def get_number():
+  while True:
+    n = int(input("Enter a value for n: "))
+    if n > 0:
+      return n
+
+
+def miaow(n):
+  for _ in range(n):
+    print("Miaow!")
+
+
+main()
