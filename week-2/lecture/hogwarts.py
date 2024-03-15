ed87fb0af7e5d57d10e416e355c1746b7b832ea1
gcpearse <glen.c.pearse@gmail.com>
2024-03-15 17:43:56 +0000

    adds lecture examples for iterating over lists and dictionaries

---
+++ week-2/lecture/hogwarts.py
@@ -0,0 +1,56 @@
+def main():
+  rank_students()
+  assign_houses()
+  parse_dict()
+
+
+def rank_students():
+  students = ["Harry", "Ron", "Hermione", "Draco"]
+  for i in range(len(students)):
+    print(f"{i + 1} {students[i]}")
+
+
+def assign_houses():
+  students = {
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Hermione": "Gryffindor",
+    "Draco": "Slytherin"
+  }
+  for key in students:
+    print(key, students[key], sep=", ")
+
+
+def parse_dict():
+  students = [
+    {
+      "name": "Harry",
+      "house": "Gryffindor",
+      "patronus": "Stag"
+    },
+    {
+      "name": "Ron",
+      "house": "Gryffindor",
+      "patronus": "Jack Russell"
+    },
+    {
+      "name": "Hermione",
+      "house": "Gryffindor",
+      "patronus": "Otter"
+    },
+    {
+      "name": "Draco",
+      "house": "Slytherin",
+      "patronus": None
+    }
+  ]
+  for student in students:
+    print(
+      student["name"], 
+      student["house"],
+      student["patronus"],
+      sep=", "
+    )
+
+
+main()
