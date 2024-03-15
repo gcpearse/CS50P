71efcc5ddff6fe4d24f8dfce22dc1daccca092c8
gcpearse <glen.c.pearse@gmail.com>
2024-03-15 18:41:43 +0000

    adds lecture examples for handling nested loops

---
+++ week-2/lecture/mario.py
@@ -0,0 +1,22 @@
+def main():
+  n = get_number()
+  print_square(n)
+
+
+def get_number():
+  while True:
+    n = int(input("Enter a number between 1 and 10: "))
+    if 0 < n <= 10:
+      return n
+
+
+def print_square(size):
+  for _ in range(size):
+    print_row(size)
+
+
+def print_row(width):
+  print("#" * width)
+
+
+main()
