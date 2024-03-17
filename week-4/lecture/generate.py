83feb12e2f05787105be8158f13a5b8a3004fc97
gcpearse <glen.c.pearse@gmail.com>
2024-03-17 22:42:10 +0000

    adds lecture examples for the random module

---
+++ week-4/lecture/generate.py
@@ -0,0 +1,26 @@
+import random
+
+
+def main():
+  flip_coin()
+  pick_number()
+  shuffle_cards(["J", "Q", "K", "A"])
+
+
+def flip_coin():
+  coins = ["heads", "tails"]
+  coin = random.choice(coins)
+  print(coin.capitalize())
+
+
+def pick_number():
+  n = random.randint(1, 10)
+  print(n)
+
+
+def shuffle_cards(cards):
+  random.shuffle(cards)
+  print(", ".join(cards))
+
+
+main()
