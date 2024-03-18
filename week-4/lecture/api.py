4f967b94bde9b1052cac10756ec394496c2db82f
gcpearse <glen.c.pearse@gmail.com>
2024-03-18 16:23:11 +0000

    amends variable names

--- week-4/lecture/api.py
+++ week-4/lecture/api.py
@@ -4,15 +4,15 @@ import sys
 
 
 def main():
-  baseUrl = "https://api.carbonintensity.org.uk/regional"
-  response = get_data(baseUrl)
+  base_url = "https://api.carbonintensity.org.uk/regional"
+  response = get_data(base_url)
   print_data(response)
 
 
-def get_data(baseUrl):
+def get_data(url):
   if (len(sys.argv)) != 2:
     sys.exit()
-  response = requests.get(f"{baseUrl}/{sys.argv[1]}")
+  response = requests.get(f"{url}/{sys.argv[1]}")
   print(f"Status code: {response.status_code}")
   return response.json()
 
