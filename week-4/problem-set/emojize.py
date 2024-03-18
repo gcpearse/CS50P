e5e490e07bb41d1a3e0ab45d094364ba544b2dda
gcpearse <glen.c.pearse@gmail.com>
2024-03-18 11:52:20 +0000

    adds completed problem set

---
+++ week-4/problem-set/emojize.py
@@ -0,0 +1,23 @@
+# Install the emoji module with pip
+# Prompt the user for input
+# Convert any codes to emoji
+# Output the result
+
+import emoji
+
+
+def main():
+  text = get_input()
+  res = convert_to_emoji(text)
+  print(f"Output: {res}")
+
+
+def get_input():
+  return input("Input: ").strip()
+
+
+def convert_to_emoji(text):
+  return emoji.emojize(text)
+
+
+main()
