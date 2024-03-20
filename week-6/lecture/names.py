651451cc9a7935e74fc12ecec081b5fbfc06c244
gcpearse <glen.c.pearse@gmail.com>
2024-03-20 22:06:13 +0000

    moves code into functions

--- week-6/lecture/names.py
+++ week-6/lecture/names.py
@@ -1,8 +1,19 @@
-names = []
+def main():
+  names = add_names_from_file()
+  greet(names)
 
-with open("names.txt") as file:
-  for line in file:
-    names.append(line.rstrip())
 
-for name in sorted(names, reverse=True):
-  print(f"Hello, {name}!")
+def add_names_from_file():
+  names = []
+  with open("names.txt") as file:
+    for line in file:
+      names.append(line.rstrip())
+  return names
+
+
+def greet(names):
+  for name in sorted(names, reverse=True):
+    print(f"Hello, {name}!")
+
+
+main()
