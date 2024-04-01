f3120e218bf0578fb91243ef15bbdb48411cf435
gcpearse <glen.c.pearse@gmail.com>
2024-04-01 17:20:53 +0100

    adds solution to problem

---
+++ week-7/problem-set/numb3rs.py
@@ -0,0 +1,22 @@
+# Expect an IPv4 address as input from the user
+# Return True is the input is a valid IPv4 address
+# Otherwise, return False
+
+import re
+
+
+def main():
+  ip = get_ip()
+  print(validate(ip))
+
+
+def get_ip():
+  return input("IPv4 Address: ").strip()
+
+
+def validate(ip):
+  return True if re.search(r"^(\d{1}|\d{2}|1\d{2}|2[0-5][0-5])\.(\d{1}|\d{2}|1\d{2}|2[0-5][0-5])\.(\d{1}|\d{2}|1\d{2}|2[0-5][0-5])\.(\d{1}|\d{2}|1\d{2}|2[0-5][0-5])$", ip) else False
+
+
+if __name__ == "__main__":
+  main()
