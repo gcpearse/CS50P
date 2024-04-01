3eb0919f420a5b11d4157c08fdab074304cbae27
gcpearse <glen.c.pearse@gmail.com>
2024-04-01 17:25:50 +0100

    adds additional test cases

--- week-7/problem-set/test/test_numb3rs.py
+++ week-7/problem-set/test/test_numb3rs.py
@@ -4,14 +4,17 @@ from numb3rs import validate
 def test_valid_ip():
   assert validate("1.2.3.4") == True
   assert validate("127.0.0.1") == True
+  assert validate("255.255.255.0") == True
   assert validate("255.255.255.255") == True
 
 
 def test_invalid_ip():
   assert validate("512.512.512.512") == False
+  assert validate("275.3.12.68") == False
   assert validate("1.2.3.1000") == False
   assert validate("1.2.3") == False
   assert validate("1.2.3.4.5") == False
+  assert validate("-1.2.3.4") == False
 
 
 def test_str():
