e21ddd12f653e6ad7a3eb1995e0a55091d6cbf03
gcpearse <glen.c.pearse@gmail.com>
2024-04-01 17:21:05 +0100

    adds test suite for numb3rs.py

---
+++ week-7/problem-set/test/test_numb3rs.py
@@ -0,0 +1,18 @@
+from numb3rs import validate
+
+
+def test_valid_ip():
+  assert validate("1.2.3.4") == True
+  assert validate("127.0.0.1") == True
+  assert validate("255.255.255.255") == True
+
+
+def test_invalid_ip():
+  assert validate("512.512.512.512") == False
+  assert validate("1.2.3.1000") == False
+  assert validate("1.2.3") == False
+  assert validate("1.2.3.4.5") == False
+
+
+def test_str():
+  assert validate("cat") == False
