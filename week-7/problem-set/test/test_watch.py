15e3b17df336281ed598e4734c836f94f78f03b6
gcpearse <glen.c.pearse@gmail.com>
2024-04-02 19:08:57 +0100

    adds additional test cases

--- week-7/problem-set/test/test_watch.py
+++ week-7/problem-set/test/test_watch.py
@@ -8,6 +8,8 @@ def test_https_url():
 
 
 def test_http_url():
+  assert parse('<iframe width="560" height="315" src="http://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>') == "https://youtu.be/xvFZjo5PgG0"
+  assert parse('<iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>') == "https://youtu.be/xvFZjo5PgG0"
   assert parse('<iframe src="http://youtube.com/embed/xvFZjo5PgG0"></iframe>') == "https://youtu.be/xvFZjo5PgG0"
 
 
