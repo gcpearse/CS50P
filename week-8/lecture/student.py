62a261480cb01c08e012e6a8ff5eded1b8f20891
gcpearse <glen.c.pearse@gmail.com>
2024-04-04 00:44:12 +0100

    adds lecture example for classes, getters, and setters

---
+++ week-8/lecture/student.py
@@ -0,0 +1,49 @@
+class Student:
+  def __init__(self, name, house):
+    self.name = name
+    self.house = house
+
+  def __str__(self):
+    return f"{self.name} is in {self.house}."
+  
+  # Getter
+  @property
+  def name(self):
+    return self._name
+  
+  # Setter
+  @name.setter
+  def name(self, name):
+    if not name:
+      raise ValueError("Missing name.")
+    self._name = name
+
+  @property
+  def house(self):
+    return self._house
+
+  @house.setter
+  def house(self, house):
+    if house not in [
+      "Gryffindor",
+      "Hufflepuff",
+      "Ravenclaw",
+      "Slytherin"
+    ]:
+      raise ValueError("Invalid house.")
+    self._house = house
+
+
+def main():
+  student = get_student()
+  print(student)
+
+
+def get_student():
+  name = input("Name: ").strip().title()
+  house = input("House: ").strip().title()
+  return Student(name, house)
+
+
+if __name__ == "__main__":
+  main()
