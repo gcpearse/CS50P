605115f1e15f7a1b16c51d7095b6d942fedb712d
gcpearse <glen.c.pearse@gmail.com>
2024-04-09 18:48:28 +0100

    adds tests for convert_to_words function

--- week-8/problem-set/test/test_seasons.py
+++ week-8/problem-set/test/test_seasons.py
@@ -1,4 +1,4 @@
-from seasons import validate_dob
+from seasons import validate_dob, convert_to_words
 
 
 def test_valid_input():
@@ -13,3 +13,13 @@ def test_invalid_input():
   assert validate_dob("1995-00-21") == False
   assert validate_dob("19950-10-11") == False
   assert validate_dob("January 1st, 1990") == False
+
+
+def test_convert_to_words():
+  assert convert_to_words(1) == "One minute"
+  assert convert_to_words(2) == "Two minutes"
+  assert convert_to_words(21) == "Twenty-one minutes"
+  assert convert_to_words(100) == "One hundred minutes"
+  assert convert_to_words(1000) == "One thousand minutes"
+  assert convert_to_words(1101) == "One thousand, one hundred one minutes"
+  assert convert_to_words(123456789) == "One hundred twenty-three million, four hundred fifty-six thousand, seven hundred eighty-nine minutes"
